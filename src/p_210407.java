852c89061ce8cc0fedf84bc1c0bcfd90edb1261a
solda <solda@khu.ac.kr>
2021-04-08 00:17:17 +0900

    updated

--- src/p_210407.java
+++ src/p_210407.java
@@ -21,11 +21,27 @@ public class p_210407 {
 //		int i2=3_000_000_000; // ERROR:-20억~20억
 //		int i3=3_000_000L; // ERROR:long>int
 //		float f=3.14; // ERROR:float<double
+//		long l=10_000_000_000; // ERROR:100억은 int를 넘음
+		long l=10_000_000_000L;
 		
 		int oct=011; // 9
 		int hex=0x11; // 17
-		System.out.println(oct);
-		System.out.println(hex);		
+		System.out.println(oct); // println()은 10진수로 출력
+		System.out.println(hex);
+		
+		System.out.println(10.0); // 10.0
+		System.out.println(.10); // 0.1
+		System.out.println(10f); // 10.0
+		System.out.println(1e3); // 1000.0
+		
+		String s1=new String("AB"); // 클래스
+		String s2="AB";
+		String s3=""; // 빈 문자열
+//		char ch=''; // ERROR:빈 문자형★
+		
+		// 문자열 + anytype, anytype + 문자열 > 문자열
+		System.out.println(""+7); // ""+"7" > "7", 숫자를 문자열로 변환
+		System.out.println(""+7+7); // "7"+"7" > "77", 왼쪽부터 변환
+		System.out.println(7+7+""); // "14", 왼쪽부터 변환
 	}
-
 }
