873f55cc165e4be9c8013f6a949a0891df6a2d03
solda <solda@khu.ac.kr>
2021-04-07 23:14:38 +0900

    updated

---
+++ src/p_210407.java
@@ -0,0 +1,23 @@
+
+public class p_210407 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		// 줄바꿈 여부
+		System.out.print("Hello Wolrd");
+		System.out.println("Hello Wolrd");
+		
+		System.out.println(5%3); // 나머지
+		System.out.println(5/3); // 1
+		System.out.println(5/3.0); // 1.6666666666666667
+
+//		char ch1="A"; // ERROR:" "is String
+		char ch2='A';
+		
+		final int MAX=100;
+		
+		 
+		
+	}
+
+}
